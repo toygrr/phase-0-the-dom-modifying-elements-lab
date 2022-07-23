cbd44e93ee0e7dc86617d03eaee9a38b249787ca
Tyler <toygrrr@gmail.com>
2022-07-23 16:12:40 -0700

    done

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const testVar = "hi"
-
-document.querySelector("main#main").remove();
+const element = document.createElement("div");
+const ul = document.createElement("ul");
+document.body.remove();
 
 const newHeader = document.createElement("h1");
-newHeader.id = "victory"
-newHeader.innerHTML = "Tyler is the champion!";
-newHeader.className = "victory";
+newHeader.id = "victory";
+newHeader.textContent = "Tyler is the champion";
